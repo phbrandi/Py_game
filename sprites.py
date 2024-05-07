af3982a3851a4406c9d9676bd34e2f1656e6f1ca
augustoda1 <augustoda@al.insper.edu.br>
2024-05-07 16:09:12 -0300

    classe tiro

--- sprites.py
+++ sprites.py
@@ -79,7 +79,6 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self, assets, bottom, centerx):
         # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
-
         self.image = assets[BULLET_IMG]
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
