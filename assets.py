56419fc20ed3104dc2f3f49d9478cfd0e65a7e70
augustoda1 <augustoda@al.insper.edu.br>
2024-05-14 16:57:03 -0300

    assets

--- assets.py
+++ assets.py
@@ -1,18 +1,24 @@
 import pygame
 import os
-from config import WIDTH, HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT, SHOT_WIDTH, SHOT_HEIGHT, IMG_DIR, SND_DIR
+from config import WIDTH, HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT, SHOT_WIDTH, SHOT_HEIGHT
 
-#SPRITES JOGADORES E TIRO
+pygame.init()
+pygame.mixer.init()
+
+# Define o diretório de imagens
+IMG_DIR = 'assets/img/'
+
+# SPRITES JOGADORES E TIRO
 P1_D = 'player1.png'
 P2_E = 'player2.png'
 P1_E = 'p1_esquerda.png'
 P2_D = 'p2_direita.png'
 TIRO_P1_D = 'tiro1.png'
 TIRO_P2_E = 'tiro2.png'
-TIRO_P1_E= 'tiro1_esquerda.png'
-TIRO_P2_D= 'tiro2_direita.png'
+TIRO_P1_E = 'tiro1_esquerda.png'
+TIRO_P2_D = 'tiro2_direita.png'
 
-#BACKGROUNDS
+# BACKGROUNDS
 INICIO = 'inicio.jpg'
 ESCOLHE_MAPA = 'Escolha_mapas.jpg'
 INSTRUCOES = 'instrucoes.jpg'
@@ -23,83 +29,73 @@ B_MAPA_4 = 'Background_4.jpg'
 VITORIA_P1 = 'vitoria1.jpg'
 VITORIA_P2 = 'vitoria2.jpg'
 
-#AUDIOS
-SND_INICIO = 'inicio.mp3'
-SND_MAPA_1 = 'mapa1.mp3'
-SND_MAPA_2 = 'mapa2.mp3'
-SND_MAPA_3 = 'mapa3.mp3'
-SND_MAPA_4 = 'mapa4.mp3'
-SND_FATALITY = 'fatality.mp3'
-
 assets = {}
-assets[INICIO] = pygame.image.load(os.path.join(IMG_DIR, INICIO)).convert()
-assets[INICIO] = pygame.transform.scale(assets[INICIO], (WIDTH, HEIGHT))
 
-assets[B_MAPA_1] = pygame.image.load(os.path.join(IMG_DIR, B_MAPA_1)).convert()
-assets[B_MAPA_1] = pygame.transform.scale(assets[B_MAPA_1], (WIDTH, HEIGHT))
+# Carregar e transformar os ativos
+INICIO = pygame.image.load(os.path.join(IMG_DIR, 'inicio.png')).convert_alpha()
+INICIO = pygame.transform.scale(INICIO, (WIDTH, HEIGHT))
+assets["INICIO"] = INICIO
 
-assets[B_MAPA_2] = pygame.image.load(os.path.join(IMG_DIR, B_MAPA_2)).convert()
-assets[B_MAPA_2] = pygame.transform.scale(assets[B_MAPA_2], (WIDTH, HEIGHT))
+ESCOLHE_MAPA = pygame.image.load(os.path.join(IMG_DIR, 'Escolha_mapas.png')).convert()
+ESCOLHE_MAPA = pygame.transform.scale(ESCOLHE_MAPA, (WIDTH, HEIGHT))
+assets["ESCOLHE_MAPA"] = ESCOLHE_MAPA
 
-assets[B_MAPA_3] = pygame.image.load(os.path.join(IMG_DIR, B_MAPA_3)).convert()
-assets[B_MAPA_3] = pygame.transform.scale(assets[B_MAPA_3], (WIDTH, HEIGHT))
+INSTRUCOES = pygame.image.load(os.path.join(IMG_DIR, 'instrucoes.png')).convert()
+INSTRUCOES = pygame.transform.scale(INSTRUCOES, (WIDTH, HEIGHT))
+assets["INSTRUCOES"] = INSTRUCOES
 
-assets[B_MAPA_4] = pygame.image.load(os.path.join(IMG_DIR, B_MAPA_4)).convert()
-assets[B_MAPA_4] = pygame.transform.scale(assets[B_MAPA_4], (WIDTH, HEIGHT))
+B_MAPA_1 = pygame.image.load(os.path.join(IMG_DIR, 'Background_1.jpg')).convert()
+B_MAPA_1 = pygame.transform.scale(B_MAPA_1, (WIDTH, HEIGHT))
+assets["B_MAPA_1"] = B_MAPA_1
 
-assets[VITORIA_P1] = pygame.image.load(os.path.join(IMG_DIR, VITORIA_P1)).convert()
-assets[VITORIA_P1] = pygame.transform.scale(assets[VITORIA_P1], (WIDTH, HEIGHT))
+B_MAPA_2 = pygame.image.load(os.path.join(IMG_DIR, 'Background_2.jpg')).convert()
+B_MAPA_2 = pygame.transform.scale(B_MAPA_2, (WIDTH, HEIGHT))
+assets["B_MAPA_2"] = B_MAPA_2
 
-assets[VITORIA_P2] = pygame.image.load(os.path.join(IMG_DIR, VITORIA_P2)).convert()
-assets[VITORIA_P2] = pygame.transform.scale(assets[VITORIA_P2], (WIDTH, HEIGHT))
+B_MAPA_3 = pygame.image.load(os.path.join(IMG_DIR, 'Background_3.jpg')).convert()
+B_MAPA_3 = pygame.transform.scale(B_MAPA_3, (WIDTH, HEIGHT))
+assets["B_MAPA_3"] = B_MAPA_3
 
-assets[P1_D] = pygame.image.load(os.path.join(IMG_DIR, P1_D)).convert()
-assets[P1_D] = pygame.transform.scale(assets[P1_D], (PLAYER_WIDTH, PLAYER_HEIGHT))
+B_MAPA_4 = pygame.image.load(os.path.join(IMG_DIR, 'Background_4.jpg')).convert()
+B_MAPA_4 = pygame.transform.scale(B_MAPA_4, (WIDTH, HEIGHT))
+assets["B_MAPA_4"] = B_MAPA_4
 
-assets[P1_E] = pygame.image.load(os.path.join(IMG_DIR, P1_E)).convert_alpha()
-assets[P1_E] = pygame.transform.scale(assets[P1_E], (PLAYER_WIDTH, PLAYER_HEIGHT))
+VITORIA_P1 = pygame.image.load(os.path.join(IMG_DIR, 'vitoria1.png')).convert()
+VITORIA_P1 = pygame.transform.scale(VITORIA_P1, (WIDTH, HEIGHT))
+assets["VITORIA_P1"] = VITORIA_P1
 
-assets[P2_E] = pygame.image.load(os.path.join(IMG_DIR, P2_E)).convert_alpha()
-assets[P2_E] = pygame.transform.scale(assets[P2_E], (PLAYER_WIDTH, PLAYER_HEIGHT))
+VITORIA_P2 = pygame.image.load(os.path.join(IMG_DIR, 'vitoria2.png')).convert()
+VITORIA_P2 = pygame.transform.scale(VITORIA_P2, (WIDTH, HEIGHT))
+assets["VITORIA_P2"] = VITORIA_P2
 
-assets[P2_D] = pygame.image.load(os.path.join(IMG_DIR, P2_D)).convert_alpha()
-assets[P2_D] = pygame.transform.scale(assets[P2_D], (PLAYER_WIDTH, PLAYER_HEIGHT))
+P1_D = pygame.image.load(os.path.join(IMG_DIR, 'player1.png')).convert()
+P1_D = pygame.transform.scale(P1_D, (PLAYER_WIDTH, PLAYER_HEIGHT))
+assets["P1_D"] = P1_D
 
-assets[TIRO_P1_D] = pygame.image.load(os.path.join(IMG_DIR, TIRO_P1_D)).convert_alpha()
-assets[TIRO_P1_D] = pygame.transform.scale(assets[TIRO_P1_D], (PLAYER_WIDTH, PLAYER_HEIGHT))
+P1_E = pygame.image.load(os.path.join(IMG_DIR, 'p1_esquerda.png')).convert_alpha()
+P1_E = pygame.transform.scale(P1_E, (PLAYER_WIDTH, PLAYER_HEIGHT))
+assets["P1_E"] = P1_E
 
-assets[TIRO_P1_E] = pygame.image.load(os.path.join(IMG_DIR, TIRO_P1_E)).convert_alpha()
-assets[TIRO_P1_E] = pygame.transform.scale(assets[TIRO_P1_E], (PLAYER_WIDTH, PLAYER_HEIGHT))
+P2_E = pygame.image.load(os.path.join(IMG_DIR, 'player2.png')).convert_alpha()
+P2_E = pygame.transform.scale(P2_E, (PLAYER_WIDTH, PLAYER_HEIGHT))
+assets["P2_E"] = P2_E
 
-assets[TIRO_P2_E] = pygame.image.load(os.path.join(IMG_DIR, TIRO_P2_E)).convert_alpha()
-assets[TIRO_P2_E] = pygame.transform.scale(assets[TIRO_P2_E], (PLAYER_WIDTH, PLAYER_HEIGHT))
+P2_D = pygame.image.load(os.path.join(IMG_DIR, 'p2_direita.png')).convert_alpha()
+P2_D = pygame.transform.scale(P2_D, (PLAYER_WIDTH, PLAYER_HEIGHT))
+assets["P2_D"] = P2_D
 
-assets[TIRO_P2_D] = pygame.image.load(os.path.join(IMG_DIR, TIRO_P2_D)).convert_alpha()
-assets[TIRO_P2_D] = pygame.transform.scale(assets[TIRO_P2_D], (PLAYER_WIDTH, PLAYER_HEIGHT))
+TIRO_P1_D = pygame.image.load(os.path.join(IMG_DIR, 'tiro1.png')).convert_alpha()
+TIRO_P1_D = pygame.transform.scale(TIRO_P1_D, (SHOT_WIDTH, SHOT_HEIGHT))
+assets["TIRO_P1_D"] = TIRO_P1_D
 
-assets[INICIO] = pygame.image.load(os.path.join(IMG_DIR, INICIO)).convert()
-assets[INICIO] = pygame.transform.scale(assets[INICIO], (WIDTH, HEIGHT))
+TIRO_P1_E = pygame.image.load(os.path.join(IMG_DIR, 'tiro1_esquerda.png')).convert_alpha()
+TIRO_P1_E = pygame.transform.scale(TIRO_P1_E, (SHOT_WIDTH, SHOT_HEIGHT))
+assets["TIRO_P1_E"] = TIRO_P1_E
 
-assets[INSTRUCOES] = pygame.image.load(os.path.join(IMG_DIR, INSTRUCOES)).convert()
-assets[INSTRUCOES] = pygame.transform.scale(assets[INSTRUCOES], (WIDTH, HEIGHT))
+TIRO_P2_E = pygame.image.load(os.path.join(IMG_DIR, 'tiro2.png')).convert_alpha()
+TIRO_P2_E = pygame.transform.scale(TIRO_P2_E, (SHOT_WIDTH, SHOT_HEIGHT))
+assets["TIRO_P2_E"] = TIRO_P2_E
 
-assets[ESCOLHE_MAPA] = pygame.image.load(os.path.join(IMG_DIR, ESCOLHE_MAPA)).convert()
-assets[ESCOLHE_MAPA] = pygame.transform.scale(assets[ESCOLHE_MAPA], (WIDTH, HEIGHT))
-
-assets[B_MAPA_1] = pygame.image.load(os.path.join(IMG_DIR, B_MAPA_1)).convert()
-assets[B_MAPA_1] = pygame.transform.scale(assets[ESCOLHE_MAPA], (WIDTH, HEIGHT))
-
-assets[B_MAPA_2] = pygame.image.load(os.path.join(IMG_DIR, B_MAPA_2)).convert()
-assets[B_MAPA_2] = pygame.transform.scale(assets[ESCOLHE_MAPA], (WIDTH, HEIGHT))
-
-assets[B_MAPA_3] = pygame.image.load(os.path.join(IMG_DIR, B_MAPA_3)).convert()
-assets[B_MAPA_3] = pygame.transform.scale(assets[ESCOLHE_MAPA], (WIDTH, HEIGHT))
-
-assets[B_MAPA_4] = pygame.image.load(os.path.join(IMG_DIR, B_MAPA_4)).convert()
-assets[B_MAPA_4] = pygame.transform.scale(assets[ESCOLHE_MAPA], (WIDTH, HEIGHT))
-
-assets[VITORIA_P1] = pygame.image.load(os.path.join(IMG_DIR, VITORIA_P1)).convert()
-assets[VITORIA_P1] = pygame.transform.scale(assets[ESCOLHE_MAPA], (WIDTH, HEIGHT))
-
-assets[VITORIA_P2] = pygame.image.load(os.path.join(IMG_DIR, VITORIA_P2)).convert()
-assets[VITORIA_P2] = pygame.transform.scale(assets[VITORIA_P2], (WIDTH, HEIGHT))
+TIRO_P2_D = pygame.image.load(os.path.join(IMG_DIR, 'tiro2_direita.png')).convert_alpha()
+TIRO_P2_D = pygame.transform.scale(TIRO_P2_D, (SHOT_WIDTH, SHOT_HEIGHT))
+assets["TIRO_P2_D"] = TIRO_P2_D
